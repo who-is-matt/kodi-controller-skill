089aedae767c3f6f40f76ca10c29e27a1c9c008f
Matt <who.is.matt.burns@gmail.com>
2018-03-14 18:14:54 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,42 +49,6 @@ class KodiControllerSkill(MycroftSkill):
             myPlayerid = player['playerid']   
         return myPlayerid
     #################################################################         
-        
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-#    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-#    def handle_hello_world_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-#        self.speak_dialog("hello.world")
-
-#    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-#    def handle_count_intent(self, message):
-#        if message.data["Dir"] == "up":
-#            self.count += 1
-#        else:  # assume "down"
-#            self.count -= 1
-#        self.speak_dialog("count.is.now", data={"count": self.count})
-
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
-    #    return False
-
     
     ### Connection controls
     @intent_handler(IntentBuilder("").require("KodiKeywords")).require("ConnectKeywords")
@@ -173,7 +137,15 @@ class KodiControllerSkill(MycroftSkill):
     def handle_direction_intent(self, message):
 #        self.speak_dialog("WIP")
         self.myKodi.AudioLibrary.Scan()     
-        
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, there is no need to override it.  If you DO
+    # need to implement stop, you should return True to indicate you handled
+    # it.
+    #
+    # def stop(self):
+    #    return False    
         
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
