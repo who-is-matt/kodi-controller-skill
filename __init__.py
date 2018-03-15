bfbf1c53f5ed94ac95374dcb8119681b8c99bffd
Matt <who.is.matt.burns@gmail.com>
2018-03-14 23:33:51 -0500

    Fixed another import issue

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
-from os.path import join
+from os.path import dirname, join
 
 from kodipydent import Kodi
 
